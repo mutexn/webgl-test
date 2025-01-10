7804e2be0d9ebb34e280a4b7bb23f611d1778342
s.kura <shoma.kura@gini.co.jp>
2025-01-11 00:23:31 +0900

    Modify

--- webgl-demo.js
+++ webgl-demo.js
@@ -1,8 +1,14 @@
 main();
 
+//
+// ここから開始
+//
 function main() {
   const canvas = document.querySelector("#glcanvas");
+  // GL コンテキストを初期化
   const gl = canvas.getContext("webgl");
+
+  // WebGL が使用可能で動作している場合にのみ続行
   if (gl === null) {
     alert(
       "WebGL を初期化できません。ブラウザーまたはマシンが対応していない可能性があります。"
@@ -10,6 +16,8 @@ function main() {
     return;
   }
 
+  // クリアカラーを黒に設定し、完全に不透明する
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // 指定されたクリアカラーでカラーバッファーをクリアする
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
